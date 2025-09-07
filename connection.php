ff821ccaf858168d94421d4bf3bd10010eafcf3e
khaliszz <122996856+khaliszz@users.noreply.github.com>
2025-09-07 22:07:38 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = "capstoneproject.mysql.database.azure.com";
-$dbname = "optimabank";
-$username = "capstone";  // ✅ just username, no @server
-$password = "Project123";
+$dbname = "";
+$username = "";  // ✅ just username, no @server
+$password = "";
 $sslcert = __DIR__ . "/DigiCertGlobalRootCA.crt.pem"; // use relative path
 
 $dsn = "mysql:host=$host;dbname=$dbname;port=3306;charset=utf8mb4";
